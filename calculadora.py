00b5c334e75892870eb7cf920a32932aa58923ea
jogimo26 <joseph.gilm@uqvirtual.edu.co>
2024-11-08 12:06:57 -0500

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -77,144 +77,140 @@ def desviacionestandar(tmuestra,muestra):
     return desvestandar
 
 print("Bienvenido al programa, por favor elija una elección:\n")
-print("1. Operaciones básicas aritméticas\n2. Complemento para funciones trigonométricas\n3. Operaciones con matrices\n4. Estadística Descriptiva\nCualquier otro número: Salir del programa")
-seleccion = int(input("Ingrese una selección para la calculadora: "))
-
+seleccion = 1
 while seleccion in [1,2,3,4]:
-        while seleccion == 1:
-            print("Escoja la operacion que desea realizar:\n")
-            print("1. Sumar 2 números\n2. Restar 2 números\n3. Multiplicar 2 números\n4. Dividir 2 números\n5. Potenciación (Base, Exponente)\n6. Raíz Cuadrada (Radicando, Precisión de la estimación)\n7. Factorial de un número\n8. Salir del menú")
-            seleccion2 = int(input("Ingrese una selección para la calculadora: "))
-            match seleccion2:
-                case 1:
-                    num1 = int(input("Ingrese el primer número para sumar: "))
-                    num2 = int(input("Ingrese el segundo número para sumar: "))
-                    suma1 = suma(num1,num2)
-                    print(f"La suma de los 2 números es: {suma1}\n")
-                case 2:
-                    num1 = int(input("Ingrese el primer número para restar: "))
-                    num2 = int(input("Ingrese el segundo número para restar: "))
-                    resta1 = resta(num1,num2)
-                    print(f"La restar de los 2 números es: {resta1}\n")
-                case 3:
-                    num1 = int(input("Ingrese el primer número para multiplicar: "))
-                    num2 = int(input("Ingrese el segundo número para multiplicar: "))
-                    mult = multiplicacion(num1,num2)
-                    print(f"El producto de los 2 números es: {mult}\n")
-                case 4:
-                    divisor = int(input("Ingrese el divisor: "))
-                    dividendo = int(input("Ingrese el dividendo: "))
-                    div = division(divisor,dividendo)
-                    print(f"La división de los 2 números es: {div}\n")
-                case 5:
-                    base = int(input("Ingrese la base para la potencia: "))
-                    exp = int(input("Ingrese el exponente para la potencia: "))    
-                    pot = potenciacion(base,exp)
-                    print(f"Su potencia es: {pot}\n")
-                case 6:
-                    rad = int(input("Ingrese el radicando para la raíz: "))
-                    precision = float(input("Ingrese la precisión con la cual calcular la raíz (mientras más cerca sea a 0, más precisa será): "))
-                    estimacion = raizcuadrada(rad,precision)
-                    print(f"La aproximación de la raiz es: {estimacion}\n")
-                case 7:
-                    numero = int(input("Ingrese el número a el cual se le va a hacer el factorial: "))
-                    fact = factorial(numero)
-                    print(f"El factorial del número es: {fact}\n")
-                case 8:
-                    print("Saliendo del menú...\n")
-                    seleccion = int(input("Ingrese una selección para la calculadora: "))
-        if seleccion == 2:
-            print("Escoja la operacion que desea realizar:\n")
-            print("1. Razón trigonométrica Sin(x)\n2. Razón trigonométrica Cos(x)\n3. Razón trigonométrica Tan(x)\n4. Salir del programa")
-            seleccion2 = int(input("Ingrese una selección para la calculadora: "))
-            match seleccion2:
-                case 1:
-                    x = float(input("Ingrese un valor para x (En grados): "))
+    print("1. Operaciones básicas aritméticas\n2. Complemento para funciones trigonométricas\n3. Operaciones con matrices de 2x2\n4. Estadística Descriptiva\nCualquier otro número: Salir del programa")
+    seleccion = int(input("Ingrese una selección para la calculadora: "))
+    print("\n")
+    if seleccion == 1:
+        print("Escoja la operacion que desea realizar:\n")
+        print("1. Sumar 2 números\n2. Restar 2 números\n3. Multiplicar 2 números\n4. Dividir 2 números\n5. Potenciación (Base, Exponente)\n6. Raíz Cuadrada (Radicando, Precisión de la estimación)\n7. Factorial de un número\n8. Salir del menú")
+        seleccion2 = int(input("Ingrese una selección para la calculadora: "))
+        match seleccion2:
+            case 1:
+                num1 = int(input("Ingrese el primer número para sumar: "))
+                num2 = int(input("Ingrese el segundo número para sumar: "))
+                suma1 = suma(num1,num2)
+                print(f"La suma de los 2 números es: {suma1}\n")
+            case 2:
+                num1 = int(input("Ingrese el primer número para restar: "))
+                num2 = int(input("Ingrese el segundo número para restar: "))
+                resta1 = resta(num1,num2)
+                print(f"La restar de los 2 números es: {resta1}\n")
+            case 3:
+                num1 = int(input("Ingrese el primer número para multiplicar: "))
+                num2 = int(input("Ingrese el segundo número para multiplicar: "))
+                mult = multiplicacion(num1,num2)
+                print(f"El producto de los 2 números es: {mult}\n")
+            case 4:
+                divisor = int(input("Ingrese el divisor: "))
+                dividendo = int(input("Ingrese el dividendo: "))
+                div = division(divisor,dividendo)
+                print(f"La división de los 2 números es: {div}\n")
+            case 5:
+                base = int(input("Ingrese la base para la potencia: "))
+                exp = int(input("Ingrese el exponente para la potencia: "))    
+                pot = potenciacion(base,exp)
+                print(f"Su potencia es: {pot}\n")
+            case 6:
+                rad = int(input("Ingrese el radicando para la raíz: "))
+                precision = float(input("Ingrese la precisión con la cual calcular la raíz (mientras más cerca sea a 0, más precisa será): "))
+                estimacion = raizcuadrada(rad,precision)
+                print(f"La aproximación de la raiz es: {estimacion}\n")
+            case 7:
+                numero = int(input("Ingrese el número a el cual se le va a hacer el factorial: "))
+                fact = factorial(numero)
+                print(f"El factorial del número es: {fact}\n")
+        if seleccion2 >= 8 or seleccion2 < 1:
+            print("Saliendo del menú...\n")
+            print("1. Operaciones básicas aritméticas\n2. Complemento para funciones trigonométricas\n3. Operaciones con matrices\n4. Estadística Descriptiva\nCualquier otro número: Salir del programa") 
+    if seleccion == 2:
+        print("Escoja la operacion que desea realizar:\n")
+        print("1. Razón trigonométrica Sin(x)\n2. Razón trigonométrica Cos(x)\n3. Razón trigonométrica Tan(x)\nCualquier otro número: Salir del programa")
+        seleccion2 = int(input("Ingrese una selección para la calculadora: "))
+        match seleccion2:
+            case 1:
+                x = float(input("Ingrese un valor para x (En grados): "))
+                while x > 360 or x < 0: # Para evitar conflictos toca hacer que los ángulos dados por el usuario estén entre 0 y 360, usando ángulos complementarios lo podemos hacer fácil
+                    if x > 360:
+                        x -= 360
+                    elif x < 0:
+                        x += 360
+                terminos = int(input("Ingrese la cantidad de términos para la cual calcular la función seno: "))
+                xrad = x*3.1415/180 # Se convierte a radianes para que funcione "bien" de 0 a 360 grados
+                aprox = seno(xrad,terminos)
+                print(f"Una aproximación de la función seno hasta {terminos} terminos es: {aprox}\n")
+            case 2:
+                x = float(input("Ingrese un valor para x (En grados): "))
+                while x > 360 or x < 0: # Lo mismo que dentro del comentario de la razón seno
+                    if x > 360:
+                        x -= 360
+                    elif x < 0:
+                        x += 360
+                terminos = int(input("Ingrese la cantidad de términos para la cual calcular la función coseno: "))
+                xrad = x*3.1415/180
+                aprox = coseno(xrad,terminos)
+                print(f"Una aproximación de la función coseno hasta {terminos} términos es: {aprox}\n")
+            case 3:
+                x = float(input("Ingrese un valor para x (En grados): "))
+                while x > 360 or x < 0:
+                    if x > 360:
+                        x -= 360
+                    elif x < 0:
+                        x += 360
+                terminos = int(input("Ingrese la cantidad de términos para la cual calcular la función tangente: "))
+                xrad = x*3.1415/180
+                aprox = tangente(xrad,terminos)
+                print(f"Una aproximación de la función tangente hasta {terminos} términos es: {aprox}\n")
+        if seleccion2 >= 8 or seleccion2 < 1:
+            print("Saliendo del menú...\n")
+            print("1. Operaciones básicas aritméticas\n2. Complemento para funciones trigonométricas\n3. Operaciones con matrices\n4. Estadística Descriptiva\nCualquier otro número: Salir del programa") 
+    if seleccion == 3:
+        print("Escoja la operacion que desea realizar:\n")
+        print("1. Suma de matrices 2x2\n2. Resta de matrices 2x2\n3. Multiplicación de matrices 2x2\n4. Determinante de una matriz 2x2\nCualquier otro número: Salir del programa")
+        seleccion2 = int(input("Ingrese una selección para la calculadora: "))
+        match seleccion2:
+            case 1:
+                continue
+            case 2:
+                continue
+            case 3:
+                continue
+            case 4:
+                continue
+        if seleccion2 >= 8 or seleccion2 < 1:
+            print("Saliendo del menú...\n")
+            print("1. Operaciones básicas aritméticas\n2. Complemento para funciones trigonométricas\n3. Operaciones con matrices\n4. Estadística Descriptiva\nCualquier otro número: Salir del programa")
+    if seleccion == 4:
+        print("Escoja la operacion que desea realizar:\n")
+        print("1. Promedio de una muestra\n2. Desviación estándar de una muestra\n3. Varianza de una muestra\nCualquier otro número: Salir del programa")
+        seleccion2 = int(input("Ingrese una selección para la calculadora: "))
+        match seleccion2:
+            case 1:
+                muestra = []
+                tmuestra = int(input("Ingrese el tamaño de la muestra que va a ingresar: "))
+                for i in range(1,tmuestra+1):
+                    datoagreg = int(input(f"Ingrese el dato {i} para agregar a la muestra: "))
+                    muestra.append(datoagreg)
+                prom = promedio(tmuestra,muestra)
+                print(f"El promedio de su muestra escogida es: {prom}\n")
+            case 2:
+                muestra = []
+                tmuestra = int(input("Ingrese el tamaño de la muestra que va a ingresar: "))
+                for i in range(1,tmuestra+1):
+                    datoagreg = int(input(f"Ingrese el dato {i} para agregar a la muestra: "))
+                    muestra.append(datoagreg)
+                desvestandar = desviacionestandar(tmuestra,muestra)
+                print(f"La desviación estándar de la muestra escogida es: {desvestandar}\n")
+            case 3:
+                muestra = []
+                tmuestra = int(input("Ingrese el tamaño de la muestra que va a ingresar: "))
+                for i in range(1,tmuestra+1):
+                    datoagreg = int(input(f"Ingrese el dato {i} para agregar a la muestra: "))
+                    muestra.append(datoagreg)
+                var = varianza(tmuestra,muestra)
+                print(f"La varianza estadística de la muestra escogida es: {var}\n")
+        if seleccion2 >= 8 or seleccion2 < 1:
+            print("Saliendo del menú...\n")
+            print("1. Operaciones básicas aritméticas\n2. Complemento para funciones trigonométricas\n3. Operaciones con matrices\n4. Estadística Descriptiva\nCualquier otro número: Salir del programa")
 
-                    while x > 360 or x < 0: # Para evitar conflictos toca hacer que los ángulos dados por el usuario estén entre 0 y 360, usando ángulos complementarios lo podemos hacer fácil
-                        if x > 360:
-                            x -= 360
-                        elif x < 0:
-                            x += 360
-
-                    terminos = int(input("Ingrese la cantidad de términos para la cual calcular la función seno: "))
-                    xrad = x*3.1415/180 # Se convierte a radianes para que funcione "bien" de 0 a 360 grados
-                    aprox = seno(xrad,terminos)
-                    print(f"Una aproximación de la función seno hasta {terminos} terminos es: {aprox}\n")
-                case 2:
-                    x = float(input("Ingrese un valor para x (En grados): "))
-
-                    while x > 360 or x < 0: # Lo mismo que dentro del comentario de la razón seno
-                        if x > 360:
-                            x -= 360
-                        elif x < 0:
-                            x += 360
-
-                    terminos = int(input("Ingrese la cantidad de términos para la cual calcular la función coseno: "))
-                    xrad = x*3.1415/180
-                    aprox = coseno(xrad,terminos)
-                    print(f"Una aproximación de la función coseno hasta {terminos} términos es: {aprox}\n")
-                case 3:
-                    x = float(input("Ingrese un valor para x (En grados): "))
-
-                    while x > 360 or x < 0:
-                        if x > 360:
-                            x -= 360
-                        elif x < 0:
-                            x += 360
-
-                    terminos = int(input("Ingrese la cantidad de términos para la cual calcular la función tangente: "))
-                    xrad = x*3.1415/180
-                    aprox = tangente(xrad,terminos)
-                    print(f"Una aproximación de la función tangente hasta {terminos} términos es: {aprox}\n")
-                case 4:
-                    print("Saliendo del menú...\n")
-                    seleccion = int(input("Ingrese una selección para la calculadora: "))       
-        if seleccion == 3:
-            print("Escoja la operacion que desea realizar:\n")
-            print("1. Suma de matrices 2x2\n2. Resta de matrices 2x2\n3. Multiplicación de matrices 2x2\n4. Determinante de una matriz 2x2\n5. Salir del programa")
-            seleccion2 = int(input("Ingrese una selección para la calculadora: "))
-            match seleccion2:
-                case 1:
-                    continue
-                case 2:
-                    continue
-                case 3:
-                    continue
-                case 4:
-                    continue
-                case 5:
-                    print("Saliendo del menú...\n")
-                    seleccion = int(input("Ingrese una selección para la calculadora: "))
-        if seleccion == 4:
-            print("Escoja la operacion que desea realizar:\n")
-            print("1. Promedio de una muestra\n2. Desviación estándar de una muestra\n3. Varianza de una muestra\n4. Salir del programa")
-            seleccion2 = int(input("Ingrese una selección para la calculadora: "))
-            match seleccion2:
-                case 1:
-                    muestra = []
-                    tmuestra = int(input("Ingrese el tamaño de la muestra que va a ingresar: "))
-                    for i in range(1,tmuestra+1):
-                        datoagreg = int(input(f"Ingrese el dato {i} para agregar a la muestra: "))
-                        muestra.append(datoagreg)
-                    prom = promedio(tmuestra,muestra)
-                    print(f"El promedio de su muestra escogida es: {prom}\n")
-                case 2:
-                    muestra = []
-                    tmuestra = int(input("Ingrese el tamaño de la muestra que va a ingresar: "))
-                    for i in range(1,tmuestra+1):
-                        datoagreg = int(input(f"Ingrese el dato {i} para agregar a la muestra: "))
-                        muestra.append(datoagreg)
-                    desvestandar = desviacionestandar(tmuestra,muestra)
-                    print(f"La desviación estándar de la muestra escogida es: {desvestandar}\n")
-                case 3:
-                    muestra = []
-                    tmuestra = int(input("Ingrese el tamaño de la muestra que va a ingresar: "))
-                    for i in range(1,tmuestra+1):
-                        datoagreg = int(input(f"Ingrese el dato {i} para agregar a la muestra: "))
-                        muestra.append(datoagreg)
-                    var = varianza(tmuestra,muestra)
-                    print(f"La varianza estadística de la muestra escogida es: {var}\n")
-                case 4:
-                    print("Saliendo del menú...\n")
-                    seleccion = int(input("Ingrese una selección para la calculadora: "))
